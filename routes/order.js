c855b05e1c791e583c82802fcc47f15b03ae00cf
imam.hermawan <imam.hermawan@gramedia.id>
2022-12-02 06:24:17 +0700

    remove middleware check role for get order by customer id'

--- routes/order.js
+++ routes/order.js
@@ -10,7 +10,7 @@ const checkRole = require('../middlewares/checkRole')
 router.get('/', Authentication, checkRole('Admin'), Order.findAllOrders)
 router.post('/', Authentication, Order.createOrder)
 router.get('/search', Authentication, Order.searchOrder)
-router.get('/customer', Authentication, checkRole('Admin'), Order.findOrderByCustomerId)
+router.get('/customer', Authentication, Order.findOrderByCustomerId)
 router.get('/:id', Authentication, Order.findOrderById)
 
 module.exports = router
